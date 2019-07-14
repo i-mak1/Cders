2484f4316c687488721e6795b3312324e0d22f64
koudai <koudai2940sura@gmail.com>
2019-07-14 14:12:07 +0900

    問い合わせ送信画面作成

--- app/controllers/admin_items_controller.rb
+++ app/controllers/admin_items_controller.rb
@@ -16,6 +16,19 @@ class AdminItemsController < ApplicationController
     @item = Items.new
   end
 
+  def create
+    @item = Items.new(item_params)
+    @item.user_id = current_user.id
+    if @item.save
+      flash[:notice] = "successfully item create"
+      redirect_to admin_items(@item.id)
+    else
+      @user = current_user
+      @items = Item.all
+      render :index
+    end
+  end
+
   def update
   end
 end
