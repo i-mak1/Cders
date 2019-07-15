41cf36d1f74499a510d4054812bdcce77b889593
koudai <koudai2940sura@gmail.com>
2019-07-15 16:28:25 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -42,22 +42,18 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "item_id"
   end
 
   create_table "contacts", force: :cascade do |t|
     t.text "contact"
     t.text "reply"
     t.boolean "is_deleted"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "disks", force: :cascade do |t|
     t.text "disk_name"
-    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -100,9 +96,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
     t.integer "stock_number"
     t.integer "item_status"
     t.boolean "is_deleted"
-    t.integer "artist_id"
-    t.integer "genre_id"
-    t.integer "label_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -117,8 +110,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
     t.integer "purchase_number"
     t.integer "purchase_price"
     t.boolean "is_deleted"
-    t.integer "item_id"
-    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -128,7 +119,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
     t.integer "order_status"
     t.integer "shipping_fee"
     t.boolean "is_deleted"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -141,7 +131,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
     t.string "shipping_postcode"
     t.text "shipping_address"
     t.boolean "is_delete"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -149,9 +138,25 @@ ActiveRecord::Schema.define(version: 2019_07_10_150122) do
   create_table "songs", force: :cascade do |t|
     t.text "song_name"
     t.integer "track"
-    t.integer "disk_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
