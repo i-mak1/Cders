f4547dfec4ca2780d2570e8088160bd1e843f7b0
koudai <koudai2940sura@gmail.com>
2019-07-15 15:49:37 +0900

    スライドショー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
 
+attachment :image
+
 has_many :carts, dependent: :destroy
 has_many :disks, dependent: :destroy
 has_many :order_details, dependent: :destroy
+
+accepts_nested_attributes_for :disks
+
 
 belongs_to :artist
 belongs_to :genre
