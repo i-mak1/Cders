36b9e889e300f5f42621d3c1219198b23cfa0c5c
koudai <koudai2940sura@gmail.com>
2019-07-18 16:51:42 +0900

    userコントローラいじりました

--- app/controllers/endusers_controller.rb
+++ app/controllers/endusers_controller.rb
@@ -10,11 +10,20 @@ class EndusersController < ApplicationController
   end
 
   def update
+    @enduser = Enduser.find(params[:id])
+    @enduser.update(user_params)
+    redirect_to enduser_path(@enduser)
   end
 
   def unsubscribe
   end
 
   def complete
+  end
+
+private
+
+  def user_params
+    params.require(:enduser).permit(:email, :first_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :tel)
   end
 end
