bfda9f8180aa2a81051626d98960284a558d861f
koudai <koudai2940sura@gmail.com>
2019-07-25 16:06:39 +0900

    降順変更

--- app/controllers/admin_orders_controller.rb
+++ app/controllers/admin_orders_controller.rb
@@ -1,7 +1,7 @@
 class AdminOrdersController < ApplicationController
   before_action :authenticate_admin!
   def index
-  	@orders = Order.all
+  	@orders = Order.all.order(id: 'DESC')
   end
 
   def show
